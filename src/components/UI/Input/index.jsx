dcbc98cda901b9f0ba0fc2dabd0b4c161900fd08
Viktor Ostrovskiy <skipeglobal@gmail.com>
2024-12-16 17:27:06 +0400

    fixed

--- src/components/UI/Input/index.jsx
+++ src/components/UI/Input/index.jsx
@@ -3,8 +3,6 @@ import './Input.scss';
 
 
 const Input = ({value, onChange, placeholder}) => {
-
-
     return (
         <div className='Input'>
             <input type="text" placeholder={placeholder} onChange={onChange} value={value}/>
